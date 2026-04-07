6937147c7c383073985557660bd61ae72b966271
vikrant123pundir <vikrant.s.pundir@gmail.com>
2026-04-07 12:58:29 +0530

    Add files via upload

---
+++ Geography_quiz/Kerala/names.js
@@ -0,0 +1,34 @@
+var map_settings = {
+        base_svg_width: 136,
+        base_svg_height: 254,
+        base_stroke_width: 1.1,
+        mod_font_size: .023,
+        mod_font_size_lables: .026,
+        bigger_map: !1
+    },
+	
+    quiz = {
+        title: "India - Andhra Pradesh - Districts",
+        hide: ["place"],
+        quiz_items: ["Mahe", "Thiruvananthapuram", "Alappuzha", "Ernakulam", "Idukki", "Kottayam", "Pathanamthitta", "Palakkad", "Malappuram", "Kozhikode", "Kannur", "Kollam", "Thrissur", "Wayanad", "Kasaragod"],
+        noquiz_regions: [],
+		lang_options: ["en"]
+    },
+	
+    questions = [
+	{item_id: "Mahe", type: "region",item_title: {en: "Mahe"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Thiruvananthapuram", type: "region",item_title: {en: "Thiruvananthapuram"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Alappuzha", type: "region",item_title: {en: "Alappuzha"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Ernakulam", type: "region",item_title: {en: "Ernakulam"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Idukki", type: "region",item_title: {en: "Idukki"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kottayam", type: "region",item_title: {en: "Kottayam"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Pathanamthitta", type: "region",item_title: {en: "Pathanamthitta"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Palakkad", type: "region",item_title: {en: "Palakkad"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Malappuram", type: "region",item_title: {en: "Malappuram"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kozhikode", type: "region",item_title: {en: "Kozhikode"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kannur", type: "region",item_title: {en: "Kannur"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kollam", type: "region",item_title: {en: "Kollam"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Thrissur", type: "region",item_title: {en: "Thrissur"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Wayanad", type: "region",item_title: {en: "Wayanad"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kasaragod", type: "region",item_title: {en: "Kasaragod"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	];
---
+++ Geography_quiz/Manipur/names.js
@@ -0,0 +1,35 @@
+var map_settings = {
+        base_svg_width: 82,
+        base_svg_height: 92,
+        base_stroke_width: 1.1,
+        mod_font_size: .023,
+        mod_font_size_lables: .026,
+        bigger_map: !1
+    },
+	
+    quiz = {
+        title: "India - Andhra Pradesh - Districts",
+        hide: ["place"],
+        quiz_items: ["Tamenglong", "Senapati", "Churachandpur", "Imphal_West", "Thoubal", "Kakching", "Kamjong", "Noney", "Bishnupur", "Imphal_East", "Chandel", "Jiribam", "Ukhrul", "Tengnoupal", "Pherzawl", "Kangpokpi"],
+        noquiz_regions: [],
+		lang_options: ["en"]
+    },
+	
+    questions = [
+	{item_id: "Tamenglong", type: "region",item_title: {en: "Tamenglong"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Senapati", type: "region",item_title: {en: "Senapati"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Churachandpur", type: "region",item_title: {en: "Churachandpur"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Imphal_West", type: "region",item_title: {en: "Imphal_West"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Thoubal", type: "region",item_title: {en: "Thoubal"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kakching", type: "region",item_title: {en: "Kakching"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kamjong", type: "region",item_title: {en: "Kamjong"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Noney", type: "region",item_title: {en: "Noney"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Bishnupur", type: "region",item_title: {en: "Bishnupur"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Imphal_East", type: "region",item_title: {en: "Imphal_East"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Chandel", type: "region",item_title: {en: "Chandel"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Jiribam", type: "region",item_title: {en: "Jiribam"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Ukhrul", type: "region",item_title: {en: "Ukhrul"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Tengnoupal", type: "region",item_title: {en: "Tengnoupal"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Pherzawl", type: "region",item_title: {en: "Pherzawl"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	{item_id: "Kangpokpi", type: "region",item_title: {en: "Kangpokpi"}, link_wikipedia: {en: "https://en.wikipedia.org/wiki/Tirupati_district"}},
+	];
